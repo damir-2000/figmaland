c460ef5e3e7880882bd30a594fe4d029510c4bb2
Damir <damir.gilemhonov.00@mail.ru>
2021-03-09 17:59:49 +0500

    adaptive

--- app/dist/script/main.js
+++ app/dist/script/main.js
@@ -434,4 +434,24 @@ class Player{
 const features__player = new Player("player");
 
 
+let colLeft = document.querySelector('.subscribe__col-left');
+let colRight = document.querySelector('.subscribe__col-right');
+let colTitle = document.querySelector('.subscribe__col-title');
+
+const mediaNotebook = window.matchMedia('(max-width: 991.98px)');
+
+mediaNotebook.addListener(media);
+
+function media(e) {
+    if (e.matches) {
+        colLeft.prepend(colTitle);
+    }
+    else{
+        colRight.prepend(colTitle);
+    }
+    
+}
+media(mediaNotebook);
+
+
 }, false);
